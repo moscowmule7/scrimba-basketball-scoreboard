f70005194be30c508f94ca6184052d89c198c721
moscowmule7 <108285408+moscowmule7@users.noreply.github.com>
2022-09-25 22:26:23 +0300

    Basketball Scoreboard Project made by using HTML, CSS and JavaScript!

---
+++ index.js
@@ -0,0 +1,42 @@
+let countHome = 0
+let countGuest = 0
+let countElHome = document.getElementById("home-score")
+let countElGuest = document.getElementById("guest-score")
+
+function addOneHome() {
+    countHome += 1
+    countElHome.textContent = countHome
+}
+
+function addTwoHome() {
+    countHome += 2
+    countElHome.textContent = countHome
+}
+
+function addThreeHome() {
+    countHome += 3
+    countElHome.textContent = countHome
+}
+
+function addOneGuest() {
+    countGuest += 1
+    countElGuest.textContent = countGuest
+}
+
+function addTwoGuest() {
+    countGuest += 2
+    countElGuest.textContent = countGuest
+}
+
+function addThreeGuest() {
+    countGuest += 3
+    countElGuest.textContent = countGuest
+}
+
+function resetHome() {
+    countElHome.textContent = 0
+}
+
+function resetGuest() {
+    countElGuest.textContent = 0
+}
